069000d21ce6c01ad1740353c168746cd88f23c7
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2022-11-03 15:58:27 +0530

    updated usermodel

--- labourzilla/myuser/models.py
+++ labourzilla/myuser/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class customuser(AbstractUser):
-    username = models.CharField(max_length = 200, null=False, unique=True)
-    email = models.EmailField(max_length = 255, null=False, unique= True)
-    contact_no = models.IntegerField(blank=True, null=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length= 255, unique=True)
+    contact_no = models.IntegerField(null=True, unique=True)
 
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
+
