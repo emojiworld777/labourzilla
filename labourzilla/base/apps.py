a02c99d83753710b1869480532809a0e84c6303a
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2022-12-20 11:27:50 +0530

    changes in app

--- labourzilla/base/apps.py
+++ labourzilla/base/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JobsConfig(AppConfig):
+class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobs'
+    name = 'base'
--- labourzilla/labourzilla/settings.py
+++ labourzilla/labourzilla/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs',
-    'myuser'
 ]
 
 MIDDLEWARE = [
@@ -123,5 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'myuser.customuser'
