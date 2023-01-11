1826a1392854b67ebefa68cee608580c511f1f2d
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2023-01-11 23:25:16 +0530

    user handling done

--- labourzilla/base/views.py
+++ labourzilla/base/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Public, Worker, Jobs
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 def home(request):
     return render(request, 'home.html')
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
     return render(request, 'login.html')
 
 def public_signup(request):
